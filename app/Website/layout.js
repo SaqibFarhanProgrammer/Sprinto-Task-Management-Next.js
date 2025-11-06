4387945791b745a4de40e10b8621369a923e6d5b
shahzaib khan <shahzaibshazzo@gmail.com>
2025-11-06 17:22:50 +0500

    ui changes

---
+++ app/Website/layout.js
@@ -0,0 +1,7 @@
+export default function WebAppLayout({ children }) {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
---
+++ app/webapp/layout.js
@@ -0,0 +1,7 @@
+export default function webapplayout ({chidren}){
+    return (
+        <div>
+            {chidren}
+        </div>
+    )
+}
